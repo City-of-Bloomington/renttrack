fddc9d6be2d3574e9eba3db02b2905214e3b2285
inghamn <inghamn@bloomington.in.gov>
2019-04-03 14:40:38 -0400

    Code formatting cleanup

--- src/main/java/in/bloomington/rental/config/AppConfig.java
+++ src/main/java/in/bloomington/rental/config/AppConfig.java
@@ -53,126 +53,115 @@ import in.bloomington.rental.model.UnitRoom;
 import in.bloomington.rental.model.StandardFees;
 
 @Configuration
-// @PropertySource({"classpath:db.properties","classpath:application.properties"})
-// on linux server
-@PropertySource({"file:/srv/data/renttrack/conf/db.properties",
-						"file:/srv/data/renttrack/conf/application.properties"})
-//
-// local machine
-// @PropertySource({"file:c:/tomcat/conf/Catalina/localhost/renttrack/db.properties","file:c:/tomcat/conf/Catalina/localhost/renttrack/application.properties"})
+@PropertySource({
+    "file:/srv/data/renttrack/conf/db.properties",
+    "file:/srv/data/renttrack/conf/application.properties"
+})
 
 @EnableTransactionManagement
 @ComponentScans(value = {
-				@ComponentScan("in.bloomington.rental.model"),				
-				@ComponentScan("in.bloomington.rental.dao"),
-				@ComponentScan("in.bloomington.rental.service"),
-				@ComponentScan("in.bloomington.rental.util")})
-public class AppConfig {
+    @ComponentScan("in.bloomington.rental.model"),
+    @ComponentScan("in.bloomington.rental.dao"),
+    @ComponentScan("in.bloomington.rental.service"),
+    @ComponentScan("in.bloomington.rental.util")
+})
 
-   @Autowired
-   private Environment env;
+public class AppConfig
+{
+    @Autowired
+    private Environment env;
 
-   @Bean
-   public LocalSessionFactoryBean getSessionFactory() {
-      LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory()
+    {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        Properties              props       = new Properties();
+        // Setting JDBC properties
+        props.put(DRIVER,  env.getProperty("postgres.driver"));
+        props.put(URL,     env.getProperty("postgres.url"));
+        props.put(USER,    env.getProperty("postgres.user"));
+        props.put(PASS,    env.getProperty("postgres.password"));
+        props.put(DIALECT, env.getProperty("postgres.dialect"));
 
-      Properties props = new Properties();
-      // Setting JDBC properties
-			/*
-      props.put(DRIVER, env.getProperty("mysql.driver"));
-      props.put(URL, env.getProperty("mysql.url"));
-      props.put(USER, env.getProperty("mysql.user"));
-      props.put(PASS, env.getProperty("mysql.password"));
-			*/
-      props.put(DRIVER, env.getProperty("postgres.driver"));
-      props.put(URL, env.getProperty("postgres.url"));
-      props.put(USER, env.getProperty("postgres.user"));
-      props.put(PASS, env.getProperty("postgres.password"));
-			props.put(DIALECT, env.getProperty("postgres.dialect"));						
-			//
-      // Setting Hibernate properties
-      props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-      props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-			// this will initialize lazy fetch
-			props.put("hibernate.enable_lazy_load_no_trans", "true");
-			
-      // Setting C3P0 pooling properties
-      props.put(C3P0_MIN_SIZE, 
-            env.getProperty("hibernate.c3p0.min_size"));
-      props.put(C3P0_MAX_SIZE, 
-            env.getProperty("hibernate.c3p0.max_size"));
-      props.put(C3P0_ACQUIRE_INCREMENT,
-            env.getProperty("hibernate.c3p0.acquire_increment"));
-      props.put(C3P0_TIMEOUT, 
-            env.getProperty("hibernate.c3p0.timeout"));
-      props.put(C3P0_MAX_STATEMENTS, 
-            env.getProperty("hibernate.c3p0.max_statements"));
+        // Setting Hibernate properties
+        props.put(SHOW_SQL,     env.getProperty("hibernate.show_sql"));
+        props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        // this will initialize lazy fetch
+        props.put("hibernate.enable_lazy_load_no_trans", "true");
 
-			// dbcp pooling
-			/*
-					 <prop key="hibernate.dbcp.initialSize">8</prop>
-            <prop key="hibernate.dbcp.maxActive">20</prop>
-            <prop key="hibernate.dbcp.maxIdle">8</prop>
-            <prop key="hibernate.dbcp.minIdle">0</prop>
-            <prop key="hibernate.dbcp.maxWait">10000</prop>
-            <prop key="hibernate.dbcp.minEvictableIdleTimeMillis">180000</prop>
-            <prop key="hibernate.dbcp.timeBetweenEvictionRunsMillis">180000</prop>
-            <prop key="hibernate.dbcp.testWhileIdle">true</prop>
-            <prop key="hibernate.dbcp.testOnReturn">true</prop>
-            <prop key="hibernate.dbcp.validationQuery">select 1</prop>
-						
-			*/
-      factoryBean.setHibernateProperties(props);
-			factoryBean.setAnnotatedClasses(Address.class,
-																			RentUser.class,
-																			Owner.class,
-																			OwnerPhone.class,
-																			PropertyType.class,
-																			BuildingType.class,
-																			RentalStatus.class,
-																			Zoning.class,
-																			InspectionType.class,
-																			PullReason.class,
-																			Rental.class,
-																			RentalOwner.class,
-																			RentalStructure.class,
-																			RentalUnit.class,
-																			Variance.class,
-																			RentalNote.class,
-																			PullHistory.class,
-																			Bill.class,
-																			Receipt.class,
-																			AttachementSeq.class,
-																			Attachement.class,
-																			LegalItEmailLog.class,
-																			RentalLog.class,
-																			Inspection.class,
-																			Can.class,
-																			Egress.class,
-																			InspectionCan.class,
-																			EmailLog.class,
-																			RentalLegal.class,
-																			EmailDetailLog.class,
-																			InspectionFileLog.class,
-																			InspectionTemplate.class,
-																			TemplateComponent.class,
-																			StandardFees.class,
-																			UnitRoom.class);			
-      return factoryBean;
-   }
+        // Setting C3P0 pooling properties
+        props.put(C3P0_MIN_SIZE,          env.getProperty("hibernate.c3p0.min_size"));
+        props.put(C3P0_MAX_SIZE,          env.getProperty("hibernate.c3p0.max_size"));
+        props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+        props.put(C3P0_TIMEOUT,           env.getProperty("hibernate.c3p0.timeout"));
+        props.put(C3P0_MAX_STATEMENTS,    env.getProperty("hibernate.c3p0.max_statements"));
 
-   @Bean
-   public HibernateTransactionManager getTransactionManager() {
-      HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-      transactionManager.setSessionFactory(getSessionFactory().getObject());
-      return transactionManager;
-   }
-		
-		@Bean(name="multipartResolver")
-		public CommonsMultipartResolver multipartResolver() {
-				CommonsMultipartResolver multi = new CommonsMultipartResolver();
-				multi.setMaxUploadSize(10000000);
-				return multi;
-		}
-		
+        // dbcp pooling
+        /*
+         * <prop key="hibernate.dbcp.initialSize">8</prop> <prop
+         * key="hibernate.dbcp.maxActive">20</prop> <prop
+         * key="hibernate.dbcp.maxIdle">8</prop> <prop
+         * key="hibernate.dbcp.minIdle">0</prop> <prop
+         * key="hibernate.dbcp.maxWait">10000</prop> <prop
+         * key="hibernate.dbcp.minEvictableIdleTimeMillis">180000</prop> <prop
+         * key="hibernate.dbcp.timeBetweenEvictionRunsMillis">180000</prop> <prop
+         * key="hibernate.dbcp.testWhileIdle">true</prop> <prop
+         * key="hibernate.dbcp.testOnReturn">true</prop> <prop
+         * key="hibernate.dbcp.validationQuery">select 1</prop>
+         *
+         */
+        factoryBean.setHibernateProperties(props);
+        factoryBean.setAnnotatedClasses(Address.class,
+                                        RentUser.class,
+                                        Owner.class,
+                                        OwnerPhone.class,
+                                        PropertyType.class,
+                                        BuildingType.class,
+                                        RentalStatus.class,
+                                        Zoning.class,
+                                        InspectionType.class,
+                                        PullReason.class,
+                                        Rental.class,
+                                        RentalOwner.class,
+                                        RentalStructure.class,
+                                        RentalUnit.class,
+                                        Variance.class,
+                                        RentalNote.class,
+                                        PullHistory.class,
+                                        Bill.class,
+                                        Receipt.class,
+                                        AttachementSeq.class,
+                                        Attachement.class,
+                                        LegalItEmailLog.class,
+                                        RentalLog.class,
+                                        Inspection.class,
+                                        Can.class,
+                                        Egress.class,
+                                        InspectionCan.class,
+                                        EmailLog.class,
+                                        RentalLegal.class,
+                                        EmailDetailLog.class,
+                                        InspectionFileLog.class,
+                                        InspectionTemplate.class,
+                                        TemplateComponent.class,
+                                        StandardFees.class,
+                                        UnitRoom.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager()
+    {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver()
+    {
+        CommonsMultipartResolver multi = new CommonsMultipartResolver();
+        multi.setMaxUploadSize(10000000);
+        return multi;
+    }
 }
